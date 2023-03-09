83fb02e9847c3b42b9f294509e34c74d54443a6c
Carlos <carlmuva@hotmail.com>
2023-03-09 02:02:28 -0600

    Se implemento FirebaseStorage y FirebaseDatabase

--- app/src/main/java/com/example/snapshots/AddFragment.kt
+++ app/src/main/java/com/example/snapshots/AddFragment.kt
@@ -1,22 +1,107 @@
 package com.example.snapshots
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import com.example.snapshots.databinding.FragmentAddBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 
 class AddFragment : Fragment() {
+    private val RC_GALLERY = 18
+    private val PATH_SNAPSHOT = "snapshots"
+
+    private lateinit var mBinding: FragmentAddBinding
+    private lateinit var mStorageReference: StorageReference
+    private lateinit var mDatabaseReference: DatabaseReference
+
+    private var mPhotoSelecteUri: Uri? = null
+
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        mBinding= FragmentAddBinding.inflate(inflater,container,false)
+        return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mBinding.btnPost.setOnClickListener { postSnapshot() }
+
+        mBinding.btnSelect.setOnClickListener { openGallery() }
+
+        mStorageReference = FirebaseStorage.getInstance().reference
+        mDatabaseReference = FirebaseDatabase.getInstance().reference.child(PATH_SNAPSHOT)
+
+
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, RC_GALLERY)
     }
 
 
+    private fun postSnapshot() {
+        mBinding.progressBar.visibility = View.VISIBLE
+        val key = mDatabaseReference.push().key!!
+        val storageReference = mStorageReference.child(PATH_SNAPSHOT).child("my_photo")
+        if(mPhotoSelecteUri != null) {
+            storageReference.putFile(mPhotoSelecteUri!!)
+                .addOnProgressListener {
+                    val progress =(100 * it.bytesTransferred/it.totalByteCount).toDouble()
+                    mBinding.progressBar.progress = progress.toInt()
+                    mBinding.tvMessage.text= "$progress%"
+                }
+                .addOnCompleteListener{
+                    mBinding.progressBar.visibility = View.INVISIBLE
+                }
+                .addOnSuccessListener{
+                    Snackbar.make(mBinding.root, "Instantánea publicada.",
+                    Snackbar.LENGTH_SHORT).show()
+                    it.storage.downloadUrl.addOnSuccessListener{
+                        saveSnapshot(key, it.toString() ,mBinding.etTitle.text.toString().trim())
+                        mBinding.tilTitle.visibility = View.GONE
+                        mBinding.tvMessage.text = getString(R.string.post_message_valid_title)
+                    }
+                }
+                .addOnFailureListener(){
+                    Snackbar.make(mBinding.root, "No se pudo subir,intente más tarde.",
+                        Snackbar.LENGTH_SHORT).show()
+                }
+        }
+    }
+
+    private fun saveSnapshot(key: String, url: String, title: String){
+        val snapshot = Snapshot(title = title, photoUrl = url)
+        mDatabaseReference.child(key).setValue(snapshot)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode==Activity.RESULT_OK) {
+            if (requestCode == RC_GALLERY) {
+                mPhotoSelecteUri = data?.data
+                mBinding.imgPhoto.setImageURI(mPhotoSelecteUri)
+                mBinding.tilTitle.visibility=View.VISIBLE
+                mBinding.tvMessage.text=getString(R.string.post_message_valid_title)
+
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/snapshots/HomeFragment.kt
+++ app/src/main/java/com/example/snapshots/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.snapshots
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -69,9 +70,11 @@ class HomeFragment : Fragment() {
                 }
             }
 
+            @SuppressLint("NotifyDataSetChanged")// Error interno fire base ui 8.0.0
             override fun onDataChanged() {
                 super.onDataChanged()
                 mBinding.progressBar.visibility = View.GONE
+                notifyDataSetChanged()
             }
 
             override fun onError(error: DatabaseError) {
